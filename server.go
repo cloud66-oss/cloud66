a845b3205e08d602e890ffaca79d6e016c07bb23
Khash Sajadi <khash@cloud66.com>
2015-02-06 17:33:47 +0000

    adding server notifications to API

--- server.go
+++ server.go
@@ -6,29 +6,36 @@ import (
 	"time"
 )
 
+type ServerNotification struct {
+	Value  interface{} `json:"value"`
+	Reason string      `json:"reason"`
+	AsOf   time.Time   `json:"asof"`
+}
+
 type Server struct {
-	Uid              string      `json:"uid"`
-	VendorUid        string      `json:"vendor_uid"`
-	Name             string      `json:"name"`
-	Address          string      `json:"address"`
-	Distro           string      `json:"distro"`
-	DistroVersion    string      `json:"distro_version"`
-	DnsRecord        string      `json:"dns_record"`
-	ServerType       string      `json:"server_type"`
-	ServerGroupId    int         `json:"server_group_id"`
-	Roles            []string    `json:"server_roles"`
-	StackUid         string      `json:"stack_uid"`
-	HasAgent         bool        `json:"has_agent"`
-	Params           interface{} `json:"params"`
-	CreatedAt        time.Time   `json:"created_at"`
-	UpdatedAt        time.Time   `json:"updated_at"`
-	Region           string      `json:"region"`
-	AvailabilityZone string      `json:"availability_zone"`
-	ExtIpV4          string      `json:"ext_ipv4"`
-	HealthCode       int         `json:"health_state"`
-	UserName         string      `json:"user_name"`
-	PersonalKey      bool        `json:"personal_key"`
-	SshPrivateKey    *string     `json:"ssh_private_key"`
+	Uid              string                        `json:"uid"`
+	VendorUid        string                        `json:"vendor_uid"`
+	Name             string                        `json:"name"`
+	Address          string                        `json:"address"`
+	Distro           string                        `json:"distro"`
+	DistroVersion    string                        `json:"distro_version"`
+	DnsRecord        string                        `json:"dns_record"`
+	ServerType       string                        `json:"server_type"`
+	ServerGroupId    int                           `json:"server_group_id"`
+	Roles            []string                      `json:"server_roles"`
+	StackUid         string                        `json:"stack_uid"`
+	HasAgent         bool                          `json:"has_agent"`
+	Params           interface{}                   `json:"params"`
+	CreatedAt        time.Time                     `json:"created_at"`
+	UpdatedAt        time.Time                     `json:"updated_at"`
+	Region           string                        `json:"region"`
+	AvailabilityZone string                        `json:"availability_zone"`
+	ExtIpV4          string                        `json:"ext_ipv4"`
+	HealthCode       int                           `json:"health_state"`
+	UserName         string                        `json:"user_name"`
+	PersonalKey      bool                          `json:"personal_key"`
+	SshPrivateKey    *string                       `json:"ssh_private_key"`
+	Notifications    map[string]ServerNotification `json:"notifications"`
 }
 
 func (s Server) Health() string {
