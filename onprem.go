2c1d08bd98e71853f4f0c078a1ea08134bd6853b
Khash Sajadi <khash@cloud66.com>
2016-11-07 17:42:20 +0000

    list onprems

--- onprem.go
+++ onprem.go
@@ -9,3 +9,13 @@ type Onprem struct {
 	CreatedAt time.Time   `json:"created_at_iso"`
 	UpdatedAt time.Time   `json:"updated_at_iso"`
 }
+
+func (c *Client) ListOnprems() ([]Onprem, error) {
+	req, err := c.NewRequest("GET", "/onprems.json", nil, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var result []Onprem
+	return result, c.DoReq(req, &result, nil)
+}
