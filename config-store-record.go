e5261a3a39b57b52b33054bf1550ce09059379b2
Dima <dima@cloud66.com>
2019-07-05 14:17:34 +0100

    Implement ConfigStore API interface

--- config-store-record.go
+++ config-store-record.go
@@ -1,5 +1,10 @@
 package cloud66
 
+import (
+	"net/url"
+	"strconv"
+)
+
 type ConfigStoreRecords struct {
 	Records []ConfigStoreRecord `json:"records" yaml:"records"`
 }
@@ -9,4 +14,95 @@ type ConfigStoreRecord struct {
 	RawValue string            `json:"raw_value" yaml:"raw_value"`
 	Metadata map[string]string `json:"metadata" yaml:"metadata"`
 	Ttl      int               `json:"ttl" yaml:"ttl"`
+}
+
+func (c *Client) GetConfigStoreRecords(namespace string) ([]ConfigStoreRecord, error) {
+	var p Pagination
+	var result []ConfigStoreRecord
+
+	query_strings := make(map[string]string)
+	query_strings["page"] = "1"
+
+	for {
+		req, err := c.NewRequest("GET", "/configstore/namespaces/"+namespace+"/records.json", nil, query_strings)
+		if err != nil {
+			return nil, err
+		}
+
+		var intermediateResult []ConfigStoreRecord
+		err = c.DoReq(req, &intermediateResult, &p)
+		if err != nil {
+			return nil, err
+		}
+		result = append(result, intermediateResult...)
+
+		if p.Current < p.Next {
+			query_strings["page"] = strconv.Itoa(p.Next)
+		} else {
+			break
+		}
+
+	}
+
+	return result, nil
+}
+
+func (c *Client) GetConfigStoreRecord(namespace, key string) (*ConfigStoreRecord, error) {
+	req, err := c.NewRequest("GET", "/configstore/namespaces/"+namespace+"/records/"+url.QueryEscape(key)+".json", nil, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var result *ConfigStoreRecord
+	err = c.DoReq(req, &result, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func (c *Client) CreateConfigStoreRecord(namespace string, record *ConfigStoreRecord) (*ConfigStoreRecord, error) {
+	req, err := c.NewRequest("POST", "/configstore/namespaces/"+namespace+"/records.json", record, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var result *ConfigStoreRecord
+	err = c.DoReq(req, &result, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func (c *Client) UpdateConfigStoreRecord(namespace, key string, record *ConfigStoreRecord) (*ConfigStoreRecord, error) {
+	req, err := c.NewRequest("PUT", "/configstore/namespaces/"+namespace+"/records/"+url.QueryEscape(key)+".json", record, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var result *ConfigStoreRecord
+	err = c.DoReq(req, &result, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func (c *Client) DeleteConfigStoreRecord(namespace, key string) (*ConfigStoreRecord, error) {
+	req, err := c.NewRequest("DELETE", "/configstore/namespaces/"+namespace+"/records/"+url.QueryEscape(key)+".json", nil, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var result *ConfigStoreRecord
+	err = c.DoReq(req, &result, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
 }
